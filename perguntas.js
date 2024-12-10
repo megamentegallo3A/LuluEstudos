78a87586c7be02c75cfb0b589cf87ff54e9594e8
megamentegallo3A <169170971+megamentegallo3A@users.noreply.github.com>
2024-12-10 16:22:10 -0300

    Projeto Finalizado

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Terror',
+    'Qual é o nome do assassino mascarado na série de filmes "Halloween"?',
+    'Michael Myers'
+)
+
+criaCartao(
+    'Terror',
+    'Em qual filme de terror um grupo de amigos é perseguido por um espírito maligno após jogar um jogo de tabuleiro?',
+    '"Ouija"'
+)
+
+criaCartao(
+    'Terror',
+    'Qual filme de terror apresenta um boneco possuído chamado Chucky?',
+    '"Brinquedo Assassino"'
+)
+
+criaCartao(
+    'Terror',
+    'Qual é o nome do demônio que assombra a família Lambert na série de filmes "Sobrenatural"?',
+    'Demônio da Face Vermelha'
+)
